8832056e902836392cbcbcf8716e16fb9018112b
dosleam <9400@holbertonstudents.com>
2024-09-03 10:41:58 +0000

    prints numbers from 0 to 99 with other code

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
-for num in range(100):
-    if num < 99:
-        print(f"{num:02}", end=", ")
+for number in range(0, 100):
+    if number != 99:
+        print("{}, ".format(number))
     else:
-        print(f"{num:02}")
+        print("{:02}".format(number), end=" ")
