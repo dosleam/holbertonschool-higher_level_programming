84292467182e7338304afd6aa1039c7877fde687
dosleam <9400@holbertonstudents.com>
2024-09-16 08:25:29 +0000

    add P0T3

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Creates a class Square
+"""
+
+
+class Square:
+
+    """
+    defines a class Square
+
+    Args:
+        size (int): the size of the new square
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Return the current square area
+        """
+        return self.__size ** 2
