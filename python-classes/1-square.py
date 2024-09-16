bc10227d6fdaa49ba4e80e20a87b2a892ec25cb8
dosleam <9400@holbertonstudents.com>
2024-09-16 08:06:04 +0000

    add P0T1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,1 +1,14 @@
-i
+#!/usr/bin/python3
+
+"""
+Creates a class Square
+"""
+
+
+class Square:
+
+    """
+    defines a class Square
+    """
+    def __init__(self.size):
+        self.__size = size
