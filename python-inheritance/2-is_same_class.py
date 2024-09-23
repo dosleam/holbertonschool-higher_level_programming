531230e8476558fe2917d757d5489fd5cafe1f47
dosleam <9400@holbertonstudents.com>
2024-09-23 08:35:35 +0000

    add P0T2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,11 +1,21 @@
 #!usr/bin/python3
 
 """
-Create a class Mylist that inherits from list
+Create a function that returns True
+if the object is exactly an instance of the specified class
+otherwise False
 """
 
 def is_same_class(obj, a_class):
-
+    """
+    Check if an object is exactly an instance
+    Args:
+        obj: object
+        a_class: class
+    return:
+        True: if object is exactly an instance of a a_class
+        False if object is not an instance of a a_class
+    """
     if type(obj) is not a_class:
         return False
     return True
