acef6b311921d7d26103d5821c90be6fa3636657
dosleam <9400@holbertonstudents.com>
2024-09-23 11:13:08 +0000

    add p0t6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Contain the class
+"""
+
+
+class BaseGeometry:
+    """
+    Contain the code
+    """
+
+    def area(self):
+        """
+        Not implemented
+        """
+
+        raise Exception("area() is not implemented")
