4835ad0975ffd0702c971772ae3c40ff491d6d17
dosleam <9400@holbertonstudents.com>
2024-09-23 08:03:16 +0000

    add P0T1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Create a class Mylist that inherits from list
+"""
+
+
+def Mylist(list):
+    """My list that inherits from list"""
+
+    def print_sorted(self):
+        """return a list sorted"""
+        print(sorted(self))
