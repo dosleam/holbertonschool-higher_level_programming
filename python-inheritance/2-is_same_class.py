ee6176b139dbb8c1411339290326961bc6c7a61b
dosleam <9400@holbertonstudents.com>
2024-09-23 08:29:45 +0000

    add P0T2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!usr/bin/python3
+
+"""
+Create a class Mylist that inherits from list
+"""
+
+def is_same_class(obj, a_class):
+
+    if type(obj) is not a_class:
+        return False
+    return True
