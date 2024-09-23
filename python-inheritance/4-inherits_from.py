5a169b830d44ee885e8a53640d9e4b1dd595da3b
dosleam <9400@holbertonstudents.com>
2024-09-23 09:41:27 +0000

    add p0t4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""
+Contain the class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Contain the code
+    """
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
