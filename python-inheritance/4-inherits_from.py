2eec05836a371fb4a75125df8df9687d174425e0
dosleam <9400@holbertonstudents.com>
2024-09-23 09:48:26 +0000

    add p0t4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,11 +6,7 @@ Contain the class
 
 
 def inherits_from(obj, a_class):
-
     """
     Contain the code
     """
-
-    if issubclass(type(obj), a_class) and type(obj) != a_class:
-        return True
-    return False
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
