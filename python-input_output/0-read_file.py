98479a34b316d2782aec64514a1b1ccd538c70a2
dosleam <9400@holbertonstudents.com>
2024-09-30 07:28:20 +0000

    add p0t1

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,5 +5,5 @@ Define function
 """
 
 def read_file(filename=""):
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         print(f.read(), end="")
