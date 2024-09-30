61d60778c4cf8b483ae8efe05959fe04247f095e
dosleam <9400@holbertonstudents.com>
2024-09-30 08:00:36 +0000

    add p0t2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+Read file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    Append a string
+
+    Args: 
+    filename: name of file
+    Text: text to append
+    """
+
+    with open(filename, 'a', encoding="utf-8") as f:
+        return f.write(text)
