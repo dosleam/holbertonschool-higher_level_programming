360059c3fd4111040ba91f3cc8343b0bd7e5408a
dosleam <9400@holbertonstudents.com>
2024-09-30 09:31:59 +0000

    add p0t8

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 
 """
-read file
+contain the code
 """
+
 import sys
+import os
+import json
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+filename = "add_item.json"
 
-try:
-    my_list = load_from_json_file("add_item.json")
-except FileNotFoundError:
-    my_list = []
-
-my_list.extend(sys.argv[1:])
-
-save_to_json_file(my_list, "add_item_json")
+if os.path.exists(filename):
+    items = load_from_json_file(filename)
+else:
+    items = []
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
