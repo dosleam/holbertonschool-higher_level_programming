f385779c54d76754d492202a6d0b7290c6da5548
dosleam <9400@holbertonstudents.com>
2024-09-30 13:43:46 +0000

    add p0t11 et 12

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -18,9 +18,8 @@ def pascal_triangle(n):
     for x in range(n):
         list_added = [1] * (x + 1)
         if x > 1:
-            for y in range (1, x):
+            for y in range(1, x):
                 list_added[y] = g_list[x - 1][y - 1] + g_list[x - 1][y]
         g_list.append(list_added)
 
     return g_list
-
