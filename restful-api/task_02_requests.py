5bb33f8ee3da179eb61e8e0816bd527530c3e4be
dosleam <9400@holbertonstudents.com>
2024-10-07 13:33:47 +0000

    fix pycodestyle

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -3,20 +3,22 @@
 import requests
 import csv
 
+
 def fetch_and_print_posts():
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
-    
     print(f'Status Code: {response.status_code}')
-    
+
     if response.status_code == 200:
         posts = response.json()
-        
+
         for post in posts:
             print(post['title'])
     else:
-        print("échec de la requte�"
+        print("échec de la requête")
+
 
 fetch_and_print_posts()
+
 
 def fetch_and_save_posts():
     response = requests.get('https://jsonplaceholder.typicode.com/posts')
@@ -24,7 +26,14 @@ def fetch_and_save_posts():
     if response.status_code == 200:
         posts = response.json()
 
-        posts_data = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+        posts_data = [
+            {
+                'id': post['id'],
+                'title': post['title'],
+                'body': post['body']
+            }
+            for post in posts
+        ]
 
         with open('posts.csv', mode='w', newline='', encoding='utf-8') as file:
             writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
@@ -32,5 +41,6 @@ def fetch_and_save_posts():
             writer.writerows(posts_data)
     else:
         print("échec de la requête")
+
 
 fetch_and_save_posts()
