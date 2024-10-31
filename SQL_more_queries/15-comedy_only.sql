b4104b8d5698ea384f2f2f3c234cc6ca59aa0383
dosleam <9400@holbertonstudents.com>
2024-10-31 09:56:42 +0100

    Add P1T15

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,4 @@
--- lists all Comedy shows in the database hbtn_0d_tvshows.
+-- lists all Comedy shows in the database hbtn_0d_tvshows
 
 SELECT tv_shows.title
     FROM tv_shows
@@ -6,6 +6,6 @@ SELECT tv_shows.title
         ON tv_shows.id = tv_show_genres.show_id
 
         INNER JOIN tv_genres
-        ON tv_shows.id = tv_show_genres.show_id
-        WHERE tv_show_genres.name = "Comedy"
+        ON tv_genres.id = tv_show_genres.genre_id
+        WHERE tv_genres.name = "Comedy"
 ORDER BY tv_shows.title;
