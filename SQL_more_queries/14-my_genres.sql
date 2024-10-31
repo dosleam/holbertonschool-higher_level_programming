209143054b06307f7ad991a2fe75d8e26d512627
dosleam <9400@holbertonstudents.com>
2024-10-31 09:46:19 +0100

    fix

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -3,7 +3,7 @@
 SELECT tv_genres.name
     FROM tv_genres
         INNER JOIN tv_show_genres
-        ON tv_genre.id = tv_show_genres.genre_id
+        ON tv_genres.id = tv_show_genres.genre_id
 
         INNER JOIN tv_shows
         ON tv_shows.id = tv_show_genres.show_id
