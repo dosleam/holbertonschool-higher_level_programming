565f9321177c7cb6b2cebb97ac7ea2117a5cc234
dosleam <9400@holbertonstudents.com>
2024-10-31 09:03:46 +0100

    fix

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
 SELECT cities.id, cities.name
 FROM cities, states
-WHERE cities.state_id = states.if AND states.name = 'California'
+WHERE cities.state_id = states.id AND states.name = 'California'
 ORDER BY cities.id ASC;
