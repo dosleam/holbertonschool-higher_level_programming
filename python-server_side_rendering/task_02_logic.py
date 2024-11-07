9d56e29b5db532f9b202d9cb56f5fd92d6dd4470
dosleam <9400@holbertonstudents.com>
2024-11-07 14:23:44 +0100

    fix

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,23 +1,19 @@
-from flask import Flask, render_template, jsonify
 import json
-
+from flask import Flask, render_template
 app = Flask(__name__)
-
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 @app.route('/items')
 def items():
-
-    try:
-        with open('items.json', 'r') as file:
-            data = json.load(file)
-            items_list = data.get("items", [])
-    except FileNotFoundError:
-        print("Erreur : Le fichier items.json est introuvable.")
-        items_list = []
-    except json.JSONDecodeError:
-        print("Erreur : Impossible de décoder le fichier JSON.")
-        items_list = []
-
-    return render_template('items.html', items=items_list)
-
+    with open('items.json', 'r') as f:
+        data = json.load(f)
+    return render_template('items.html', items=data['items'])
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
