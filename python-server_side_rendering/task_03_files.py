4a8917e9e1fc95df94a1d4865babd5bba80a5d6e
dosleam <9400@holbertonstudents.com>
2024-11-08 09:55:24 +0100

    fix

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -34,30 +34,41 @@ def items():
 
 @app.route('/products', methods=['GET'])
 def display_products():
-
+    # récupère la source (json, csv, ou une source invalide)
     source = request.args.get('source', '')
-
+    # récupère l'id
     product_id = request.args.get('id', None)
     message = None
     products = []
 
+    # Si la source n'est pas json ou csv
     if source not in ['json', 'csv']:
         message = "Wrong source"
 
     try:
         if source == 'json':
+            # ouvre le fichier json et assigne le contenu à products
             with open('products.json', 'r') as file:
                 products = json.load(file)
 
         elif source == 'csv':
+            # ouvre le fichier csv et convertie les infos en dict
+            # avant de l'écrire dans la variable reader
             with open('products.csv', 'r') as file:
                 reader = csv.DictReader(file)
+                # créer une liste contenant chaque ligne
+                # du CSV sous forme de dictionnaire
                 products = [row for row in reader]
 
+        # Si id n'est pas NULL (donc si un id a été fournis)
         if product_id:
+            # remplace la liste products par une nouvelle liste
+            # contenant le product dans products qui a une clé "id"
+            # correspondant à product_id
             products = [
                 product for product in products if str(
                     product['id']) == product_id]
+            # Si la correspondance n'existe pas
             if not products:
                 message = "Product not found."
 
